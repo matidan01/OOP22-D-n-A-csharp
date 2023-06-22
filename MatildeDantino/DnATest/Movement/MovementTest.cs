49a2734ba01f9b524f1f436d00faccac4c03cd7f
matidan01 <matildedantino@gmail.com>
2023-06-22 15:05:56 +0200

    Small changes in AbstractEntity, AbstractMovableEntity and MovementTest

--- MatildeDantino/DnATest/Movement/MovementTest.cs
+++ MatildeDantino/DnATest/Movement/MovementTest.cs
@@ -30,21 +30,21 @@ public class MovementTest
     {
         // Test the "right" command
         ICommandFactory _command = new CommandFactory(_player);
-        Vector2d expectedVectorRight = new Vector2d(IPlayer.STANDARDVELOCITY, 0);
+        Vector2d expectedVectorRight = new (IPlayer.STANDARDVELOCITY, 0);
         (State.StateEnum, State.StateEnum) expectedStateRight = (State.StateEnum.STATE_STANDING, State.StateEnum.STATE_RIGHT);
         _command.Right().Execute();
         Assert.AreEqual(expectedVectorRight, _player.GetVector());
         Assert.AreEqual(expectedStateRight, _player.GetStateCopy().GetPairState());
 
         // Test the "left" command
-        Vector2d expectedVectorLeft = new Vector2d(-IPlayer.STANDARDVELOCITY, 0);
+        Vector2d expectedVectorLeft = new (-IPlayer.STANDARDVELOCITY, 0);
         (State.StateEnum, State.StateEnum) expectedStateLeft = (State.StateEnum.STATE_STANDING, State.StateEnum.STATE_RIGHT);
         _command.Left().Execute();
         Assert.AreEqual(expectedVectorLeft, _player.GetVector());
         Assert.AreEqual(expectedStateLeft, _player.GetStateCopy().GetPairState());
 
         // Test the "jump" command
-        Vector2d expectedVectorJump = new Vector2d(0, -IPlayer.JUMPVELOCITY);
+        Vector2d expectedVectorJump = new (0, -IPlayer.JUMPVELOCITY);
         (State.StateEnum, State.StateEnum) expectedStateJump = (State.StateEnum.STATE_STANDING, State.StateEnum.STATE_RIGHT);
         _command.Stop().Execute();
         _command.Jump().Execute();
